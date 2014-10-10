4f4153a0a11f3e9765b35ab588c604ebda149cbb
acmadmin <acmadmin@acm-styler-mbp-3.local>
2014-10-10 11:29:55 -0500

    First rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Postcard
 //
-//  Created by acmadmin on 9/25/14.
+//  Created by Stephen Tyler on 9/25/14.
 //  Copyright (c) 2014 Stphntylr. All rights reserved.
 //
 
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
 
 }
 
